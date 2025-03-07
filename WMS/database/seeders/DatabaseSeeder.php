7d99f46475452a02b172a45a4b2bd429cb73c27b
Radin-Mokari <rmokari@bradford.ac.uk>
2025-03-07 15:25:54 +0000

    User Management integartion

--- WMS/database/seeders/DatabaseSeeder.php
+++ WMS/database/seeders/DatabaseSeeder.php
@@ -16,15 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // Create a default admin user
-        User::create([
-            'first_name' => 'Admin',
-            'last_name' => 'User',
-            'username' => 'admin',
-            'email' => 'admin@example.com',
-            'phone_number' => '1234567890',
-            'password' => Hash::make('password'),
-            'role' => 'admin',
+        $this->call([
+            AdminUserSeeder::class,
         ]);
     }
 }
--- WMS/routes/web.php
+++ WMS/routes/web.php
@@ -9,83 +9,79 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\SupervisorController;
 use App\Http\Controllers\StaffController;
+use App\Http\Controllers\UserController;
 
-
+// Public routes
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
-
-// Admin dashboard
-Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-
-Route::get('/supervisor/dashboard', [SupervisorController::class, 'dashboard'])->name('supervisor.dashboard');
-
-// Projects Routes
-Route::get('projects/create', [ProjectController::class, 'create'])->name('projects.create');
-Route::post('projects/store-data', [ProjectController::class, 'storeProjectData'])->name('projects.store-data');
-Route::post('projects', [ProjectController::class, 'store'])->name('projects.store');
-
-Route::get('projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
-Route::get('projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-Route::put('projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
-Route::delete('projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
-
-// Tasks Routes
-Route::get('tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
-
-Route::get('tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
-Route::get('tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-Route::put('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-
-
-//Route for Staff tasks
-
-Route::get('/staff/dashboard', [StaffController::class, 'dashboard'])->name('staff.dashboard');
-
-
-//User Management routes
-Route::get('/users', [App\Http\Controllers\UserController::class, 'indexView'])->name('admin.user_management.index');
-Route::post('/users', [App\Http\Controllers\UserController::class, 'store'])->name('admin.user_management.store');
-Route::get('/users/{user}', [App\Http\Controllers\UserController::class, 'show'])->name('admin.user_management.show');
-Route::put('/users/{user}', [App\Http\Controllers\UserController::class, 'update'])->name('admin.user_management.update');
-Route::delete('/users/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->name('admin.user_management.destroy');
-
-
-Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
-    ->name('password.request');
-
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-    ->name('password.email');
-
-Route::get('/reset-password/{token}', [PasswordResetLinkController::class, 'edit'])
-    ->name('password.reset');
-
-Route::post('/reset-password', [PasswordResetLinkController::class, 'update'])
-    ->name('password.update');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update-profile-information-form'])->name('profile.update-profile-information-form');
+// Guest routes
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
     
+    Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
+        ->name('password.request');
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->name('password.email');
+    Route::get('/reset-password/{token}', [PasswordResetLinkController::class, 'edit'])
+        ->name('password.reset');
+    Route::post('/reset-password', [PasswordResetLinkController::class, 'update'])
+        ->name('password.update');
 });
 
+// Protected routes
+Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    
+    // Admin routes
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+        
+        // User Management routes
+        Route::prefix('users')->name('user_management.')->group(function () {
+            Route::get('/', [UserController::class, 'indexView'])->name('index');
+            Route::post('/', [UserController::class, 'store'])->name('store');
+            Route::get('/{user}', [UserController::class, 'show'])->name('show');
+            Route::put('/{user}', [UserController::class, 'update'])->name('update');
+            Route::delete('/{user}', [UserController::class, 'destroy'])->name('destroy');
+        });
+    });
 
-/*
-// Admin User Management Routes
-Route::prefix('admin/user_management')->name('admin.user_management.')->group(function () {
-    Route::get('/users', [App\Http\Controllers\UserController::class, 'indexView'])->name('admin.user_management.index');
-    Route::post('/users', [App\Http\Controllers\UserController::class, 'store'])->name('admin.user_management.store');
-    Route::get('/users/{user}', [App\Http\Controllers\UserController::class, 'show'])->name('admin.user_management.show');
-    Route::put('/users/{user}', [App\Http\Controllers\UserController::class, 'update'])->name('admin.user_management.update');
-    Route::delete('/users/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->name('admin.user_management.destroy');
-}); */
+    // Supervisor routes
+    Route::get('/supervisor/dashboard', [SupervisorController::class, 'dashboard'])->name('supervisor.dashboard');
 
+    // Staff routes
+    Route::get('/staff/dashboard', [StaffController::class, 'dashboard'])->name('staff.dashboard');
+
+    // Projects Routes
+    Route::prefix('projects')->name('projects.')->group(function () {
+        Route::get('/create', [ProjectController::class, 'create'])->name('create');
+        Route::post('/store-data', [ProjectController::class, 'storeProjectData'])->name('store-data');
+        Route::post('/', [ProjectController::class, 'store'])->name('store');
+        Route::get('/{project}', [ProjectController::class, 'show'])->name('show');
+        Route::get('/{project}/edit', [ProjectController::class, 'edit'])->name('edit');
+        Route::put('/{project}', [ProjectController::class, 'update'])->name('update');
+        Route::delete('/{project}', [ProjectController::class, 'destroy'])->name('destroy');
+    });
+
+    // Tasks Routes
+    Route::prefix('tasks')->name('tasks.')->group(function () {
+        Route::get('/create', [TaskController::class, 'create'])->name('create');
+        Route::post('/', [TaskController::class, 'store'])->name('store');
+        Route::get('/{task}', [TaskController::class, 'show'])->name('show');
+        Route::get('/{task}/edit', [TaskController::class, 'edit'])->name('edit');
+        Route::put('/{task}', [TaskController::class, 'update'])->name('update');
+        Route::delete('/{task}', [TaskController::class, 'destroy'])->name('destroy');
+    });
+
+    // Profile routes
+    Route::prefix('profile')->name('profile.')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+        Route::patch('/', [ProfileController::class, 'update-profile-information-form'])->name('update-profile-information-form');
+    });
+});
 
 require __DIR__.'/auth.php';
 
